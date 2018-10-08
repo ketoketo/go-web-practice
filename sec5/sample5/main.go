55b0eb7c18d96885d965c946d2396698c6059189
tMatSuZ <tmatsuzaki1@gmail.com>
2018-10-08 12:39:02 +0900

    template内での変数定義方法

---
+++ sec5/sample5/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+	m := map[string]string{"matsu": "tat", "ta": "ma", "ki": "n"}
+	t.Execute(w, m)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "localhost:8080",
+	}
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
