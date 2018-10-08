2c526e4fe48ccd7030b56a221d768cf4803ad61d
tMatSuZ <tmatsuzaki1@gmail.com>
2018-10-08 12:23:41 +0900

    template html追加

---
+++ sec5/sample3/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+	// daysOfWeek := []string{"月", "火", "日"}
+	daysOfWeek := []string{}
+	t.Execute(w, daysOfWeek)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "localhost:8080",
+	}
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
