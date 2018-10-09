11e2da0942838c6b36f96e99ac66ce5bfb714ae9
tMatSuZ <tmatsuzaki1@gmail.com>
2018-10-10 01:00:53 +0900

    goroutine sample追加

--- sec9/sample1/goroutine_test.go
+++ sec9/sample1/goroutine_test.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"time"
 	"testing"
+	"time"
 )
 
 func TestPrint1(t *testing.T) {
@@ -12,4 +12,21 @@ func TestPrint1(t *testing.T) {
 func TestGoPrint1(t *testing.T) {
 	goPrint1()
 	time.Sleep(1 * time.Millisecond)
+}
+
+func TestGoPrint2(t *testing.T) {
+	goPrint2()
+	time.Sleep(2 * time.Millisecond)
+}
+
+func BenchmarkPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print1()
+	}
+}
+
+func BenchmarkPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint1()
+	}
 }
