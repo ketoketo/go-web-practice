2c730e89f634e010f15bebcbcf4a0a6061b6cd6d
tMatSuZ <tmatsuzaki1@gmail.com>
2018-10-12 01:01:24 +0900

    モザイク１

---
+++ sec9/sample7/mosaic.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"image"
+	"net/http"
+)
+
+func averageColor(img image.Image) [3]float64 {
+	bounds := img.Bounds()
+	r, g, b := 0.0, 0.0, 0.0
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r1, g1, b1, _ := img.At(x, y).RGBA()
+			r, g, b = r+float64(r1), g+float64(g1), b+float64(b1)
+		}
+	}
+	totalPixels := float64(bounds.Max.X * bounds.Max.Y)
+	return [3]float64{r / totalPixels, g / totalPixels, b / totalPixels}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+}
